e45ab8f9809a6ebc53d0a4b4ae9e2413220da881
Ma. Bernadete Baroto <bernadetebaroto@gmail.com>
2021-11-27 05:45:11 -0800

    ask for a sentence and display the number of words

---
+++ programfile1.py
@@ -0,0 +1,17 @@
+# ask for a sentence input
+
+sentence = input('Enter a sentence: ')
+
+
+# display the number of words
+count = 0
+for c in sentence:
+    if c == ' ':
+        count = count+1
+print(count)
+
+
+
+
+# display the number of vowels
+# display the number of consonants
