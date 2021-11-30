7c85f41d9e25e7732f068d350892d95ac7f9bec5
Ma. Bernadete Baroto <bernadetebaroto@gmail.com>
2021-11-30 03:40:38 -0800

    updated reasons for passwords to be invalid

--- programfile2.py
+++ programfile2.py
@@ -10,7 +10,7 @@ char3 = '!@#$%^&*()_+{}:"|<>?[]\;/.,=-+'
 # check if the password is greater than 15 letters
 
 if len(password) <= 15:
-    print('password not valid!')
+    print('password not valid! must have at least 16 characters.')
 
 # have at least one capital letter
 def count_char(char1, password):
@@ -23,7 +23,7 @@ def count_char(char1, password):
 capital = count_char(char1, password)
 
 if capital == 0:
-    print('password not valid!')
+    print('password not valid! must have at least one capital.')
 
 # have at least one number
 def count_char(char2, password):
@@ -36,7 +36,7 @@ def count_char(char2, password):
 number = count_char(char2, password)
 
 if number == 0:
-    print('password not valid!')
+    print('password not valid! must have at least one number.')
 
 # have at least one special character
 def count_char(char3, password):
@@ -49,13 +49,13 @@ def count_char(char3, password):
 special = count_char(char3, password)
 
 if special == 0:
-    print('password not valid!')
+    print('password not valid! must have at least one special character.')
 
 # check if all conditions were satisfied then password is valid
 if len(password) > 15:
     if capital > 0:
         if number > 0:
             if special > 0:
-                print('VALID PASSWORD!')
+                print('VALID PASSWORD!') 
 
 
