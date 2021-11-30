5458eea9f23dac4ee95887c3856985c3dea68fcf
Ma. Bernadete Baroto <bernadetebaroto@gmail.com>
2021-11-29 17:53:24 -0800

    have at least one capital letter done

--- programfile2.py
+++ programfile2.py
@@ -2,13 +2,26 @@
 # ask for a input password
 
 password = input('Enter your password: ')
+char1 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 # check if the password is valid
 # check if the password is greater than 15 letters
 
 if len(password) <= 15:
     print('password not valid!')
-    
+
 # have at least one capital letter
+def count_char(char1, password):
+    count = 0
+    for c in password:
+        for d in char1:
+            if c == d:
+                count = count+1
+    return count
+capital = count_char(char1, password)
+
+if capital == 0:
+    print('password not valid!')
+
 # have at least one number
 # have at least one special character
