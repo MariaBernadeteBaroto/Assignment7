0a8a9554dbe7052469dab2960421d3b126c37354
Ma. Bernadete Baroto <bernadetebaroto@gmail.com>
2021-11-29 17:38:09 -0800

    check if the password is greater than 15 character

--- programfile2.py
+++ programfile2.py
@@ -1,7 +1,14 @@
 # Program 2: Password validator
 # ask for a input password
+
+password = input('Enter your password: ')
+
 # check if the password is valid
 # check if the password is greater than 15 letters
+
+if len(password) <= 15:
+    print('password not valid!')
+    
 # have at least one capital letter
 # have at least one number
 # have at least one special character
