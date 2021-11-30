a8b19a912b8957e2004e297bd0fd94829e3fc45d
Ma. Bernadete Baroto <bernadetebaroto@gmail.com>
2021-11-29 17:24:00 -0800

    the whole program is done and is working properly

--- programfile1.py
+++ programfile1.py
@@ -3,12 +3,13 @@
 text = input('Enter a sentence: ')
 char1 = ' '
 char2 = 'aeiouAEIOU'
+char3 = 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'
 
 
 # display the number of words
 
 def count_char(char1, text):
-    count = 0
+    count = 1
     for c in text:
         for d in char1:
             if c == d:
@@ -29,8 +30,22 @@ def count_char(char2, text):
 
 vowels = count_char(char2, text)
 
-print(f'number of words are {words}')
-print(f'the number of vowels are {vowels}:')
-
 
 # display the number of consonants
+
+def count_char(char3, text):
+    count = 0
+    for c in text:
+        for d in char3:
+            if c == d:
+                count = count+1
+    return count
+
+consonants = count_char(char3, text)
+
+
+print(f'number of words: {words}')
+print(f'number of vowels: {vowels}:')
+print(f'number of consonants: {consonants}')
+
+
