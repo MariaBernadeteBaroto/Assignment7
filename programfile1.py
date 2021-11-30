092585073f2eda9bb7ebcbf85453799fd2b8c154
Ma. Bernadete Baroto <bernadetebaroto@gmail.com>
2021-11-29 16:50:22 -0800

    counting the number of words done

--- programfile1.py
+++ programfile1.py
@@ -1,17 +1,24 @@
 # ask for a sentence input
 
-sentence = input('Enter a sentence: ')
+text = input('Enter a sentence: ')
+char = ' '
 
 
 # display the number of words
-count = 0
-for c in sentence:
-    if c == ' ':
-        count = count+1
-print(count)
 
+def count_char(char, text):
+    count = 0
+    for c in text:
+        for d in char:
+            if c == d:
+                count = count+1
+    return count
 
+words = count_char(char, text)
+
+print(words)
 
 
 # display the number of vowels
+
 # display the number of consonants
