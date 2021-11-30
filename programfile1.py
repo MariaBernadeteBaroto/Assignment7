593925fd1c0ee696ddfb662686d03a3a0e6798a7
Ma. Bernadete Baroto <bernadetebaroto@gmail.com>
2021-11-29 17:07:24 -0800

    done coutnting the vowels and dubugging the prog

--- programfile1.py
+++ programfile1.py
@@ -1,24 +1,36 @@
 # ask for a sentence input
 
 text = input('Enter a sentence: ')
-char = ' '
+char1 = ' '
+char2 = 'aeiouAEIOU'
 
 
 # display the number of words
 
-def count_char(char, text):
+def count_char(char1, text):
     count = 0
     for c in text:
-        for d in char:
+        for d in char1:
             if c == d:
                 count = count+1
     return count
 
-words = count_char(char, text)
-
-print(words)
-
+words = count_char(char1, text)
 
 # display the number of vowels
+
+def count_char(char2, text):
+    count = 0
+    for c in text:
+        for d in char2:
+            if c == d:
+                count = count+1
+    return count
+
+vowels = count_char(char2, text)
+
+print(f'number of words are {words}')
+print(f'the number of vowels are {vowels}:')
+
 
 # display the number of consonants
