a70af76d9b9bc7c1ebebff8705fea2ce6071c9f7
Ma. Bernadete Baroto <bernadetebaroto@gmail.com>
2021-11-29 17:30:03 -0800

    program steps

---
+++ programfile2.py
@@ -0,0 +1,7 @@
+# Program 2: Password validator
+# ask for a input password
+# check if the password is valid
+# check if the password is greater than 15 letters
+# have at least one capital letter
+# have at least one number
+# have at least one special character
